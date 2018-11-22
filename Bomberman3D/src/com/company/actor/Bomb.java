2cec682dd7469dc80a4bfbe84bd366024475ef73
DO THI HUONG <dothihuong273@gmail.com>
2018-11-23 00:54:10 +0700

    change folder in 23/11/2018

---
+++ Bomberman3D/src/com/company/actor/Bomb.java
@@ -0,0 +1,93 @@
+package com.company.actor;
+
+import java.awt.Rectangle;
+
+import javax.swing.ImageIcon;
+
+public class Bomb extends Actor{
+	//	protected int x,y,type,orient, speed, width, height, runBomb;
+//	protected Image img;
+//	protected int x,y,size,widthBomb, heightBomb,timeline,type;
+//	protected Image img;
+	protected int size, timeline;
+
+	public Bomb(int x, int y, int size, int timeline){
+		x=(x/45)*45;
+		y=(y/45)*45;
+		this.x=x;
+		this.y=y;
+		this.size=size;
+		this.orient=0;			//định hướng
+		this.timeline = timeline;
+		this.typeActor = Actor.BOMB;
+		img= new ImageIcon(getClass().getResource("/Images/bomb.gif")).getImage();			//sử dụng ImageIcon để hiển thị bomb trên background_Play
+		this.width= img.getWidth(null);
+		this.height= img.getHeight(null);
+	}
+
+	public Bomb(int x, int y,int orient, int speed, int size, int timeline){
+		x=(x/45)*45;
+		y=(y/45)*45;
+		this.x=x;
+		this.y=y;
+		this.orient = orient;
+		this.speed = 5;
+		this.size=size;
+		this.timeline = timeline;
+		this.typeActor = Actor.BOMB;
+		img= new ImageIcon(getClass().getResource("/Images/bomb.gif")).getImage();
+		this.width = img.getWidth(null);
+		this.height = img.getHeight(null);
+	}
+
+	public void deadlineBomb(){
+		this.timeline--;
+	}
+
+	public int getTimeline() {
+		return timeline;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public boolean setRun(Bomber bomber){
+		Rectangle rec2 = new Rectangle(x, y, 45, 45);
+		Rectangle rec3 = new Rectangle(bomber.getX(), bomber.getY(), bomber.getWidth(), bomber.getHeight());
+		return rec2.intersects(rec3);
+	}
+
+	public void setTimeline(int timeline) {
+		this.timeline = timeline;
+	}
+
+
+	public boolean isImpact(int xNewBomb, int yNewBomb){
+		Rectangle rec1 = new Rectangle(x, y, 45, 45);
+		Rectangle rec2 = new Rectangle(xNewBomb, yNewBomb, 45, 45);
+		return rec1.intersects(rec2);
+	}
+
+
+	public int isImpactBombvsActor(Actor actor){
+		if(actor.getRunBomb()==Bomber.ALLOW_RUN) {
+			return 0;
+		}
+		Rectangle rec2 = new Rectangle(x, y, 45, 45);
+		Rectangle rec3 = new Rectangle(actor.getX(), actor.getY(), actor.getWidth(), actor.getHeight());
+		if(rec2.intersects(rec3)){
+			if(actor.getType()== Bomber.BOSS) {
+				return 2;
+			}
+//			else if(actor.getType()==Bomber.SOOT) {
+//				return 3;
+//			}
+//			else if(actor.getType()==Bomber.PIRATE) {
+//				return 4;
+//			}
+			return 1;
+		}
+		return 0;
+	}
+}
