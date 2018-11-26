7bb54d0d37810d29371dfd7f23f082c5231d5646
DO THI HUONG <dothihuong273@gmail.com>
2018-11-26 13:08:58 +0700

    change folder

---
+++ Bomberman3D_26_11_2018/src/com/company/actor/HightScore.java
@@ -0,0 +1,30 @@
+package com.company.actor;
+
+public class HightScore {
+	private String name;
+	private int score;
+
+	public HightScore(String name, int score) {
+		super();
+		this.name = name;
+		this.score = score;
+	}
+
+    public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+	
+}
